ae997ff8f29b8241abc4da893ed03e6ca02cd05c
DonDuala <john@dual.finance>
2025-01-04 16:17:17 -0400

    Downgrade manifest sdk 0.159

--- src/tools/batch_order.ts
+++ src/tools/batch_order.ts
@@ -8,8 +8,8 @@ import { SolanaAgentKit } from "../index";
 import {
   ManifestClient,
   WrapperPlaceOrderParamsExternal,
+  OrderType,
 } from "@cks-systems/manifest-sdk";
-import { OrderType } from "@cks-systems/manifest-sdk/client/ts/src/wrapper/types/OrderType";
 
 export interface OrderParams {
   quantity: number;
