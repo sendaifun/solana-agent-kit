f4e49fd41f8707c4a859f33b17b4f5283120415d
Noushad Chaudhary <139552786+0xNoushad@users.noreply.github.com>
2025-03-05 18:06:08 +0530

    Update index.ts

--- src/langchain/index.ts
+++ src/langchain/index.ts
@@ -1,6 +1,7 @@
 export * from "./adrena";
 export * from "./alldomains";
 export * from "./dexscreener";
+export * from "./alldomains";
 export * from "./flash";
 export * from "./gibwork";
 export * from "./jupiter";
@@ -287,7 +288,6 @@ export function createSolanaTools(solanaKit: SolanaAgentKit) {
     new ElfaGetTopMentionsTool(solanaKit),
     new ElfaAccountSmartStatsTool(solanaKit),
     new SolanaFluxbeamCreatePoolTool(solanaKit),
-    new SolanaApproveProposal2by2Multisig(solanaKit),
     new SolanaCreateRealmTool(solanaKit),
     new SolanaCreateProposalTool(solanaKit),
     new SolanaCastVoteTool(solanaKit),
